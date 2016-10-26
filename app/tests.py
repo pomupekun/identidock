3b5186efa4d670b32c6fe8c4b30e82c586b30f69
pomupekun <pomupekun@gmail.com>
2016-10-26 23:45:44 +0900

    add unit tests

--- app/tests.py
+++ app/tests.py
@@ -12,10 +12,10 @@ class TestCase(unittest.TestCase):
         assert page.status_code == 200
         assert 'Hello' in str(page.data)
         assert 'Moby Dock' in str(page.data)
+
     def test_html_escaping(self):
         page = self.app.post("/", data=dict(name='"><b>TEST</b><!--"'))       
         assert '<b>' not in str(page.data)
         
 if __name__ == '__main__':
     unittest.main()
-
